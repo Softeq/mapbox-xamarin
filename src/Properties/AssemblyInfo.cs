95e24f1b037baadbab5947562f92f9e107730cc6
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2021-06-18 02:43:24 +0300

    Add nuspec

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -12,20 +12,20 @@ using Foundation;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("NativeLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Softeq.MapBoxBindingIOS")]
+[assembly: AssemblyDescription("Xamarin.iOS binding library for Mapbox SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NativeLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Softeq Development Corp.")]
+[assembly: AssemblyProduct("MapBoxBindingIOS")]
+[assembly: AssemblyCopyright("Copyright 2021 Softeq Development Corp.")]
+[assembly: AssemblyTrademark("Softeq")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("6.2.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
